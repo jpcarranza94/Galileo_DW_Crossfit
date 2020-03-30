3b41e4330db6726d7e2efee941f86c49291bc629
AndreRodas <andrejosue.rh@gmail.com>
2020-03-30 16:15:29 -0600

    V1.6
    Ya con el 'athlete_session_per_day' corregido

--- schema_v0.1.sql
+++ schema_v0.1.sql
@@ -131,7 +131,8 @@ Delimiter //
 CREATE TRIGGER athlete_session_per_day
     BEFORE INSERT ON session FOR EACH ROW
     BEGIN
-        IF ((SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=new.id_class AND a.id_athlete=new.id_athlete) IS NOT NULL) THEN
+        IF ((SELECT s.id_session FROM session as s JOIN class as c ON s.id_class=c.id_class JOIN athlete as a ON s.id_athlete = a.id_athlete
+        WHERE a.id_athlete = new.id_athlete AND c.id_class = new.id_class) IS NOT NULL) THEN
            signal sqlstate '45000' SET MESSAGE_TEXT = 'Solamente se puede una clase por dia';
         END IF;
     END;
@@ -159,13 +160,35 @@ INSERT INTO  session(id_class,hour) VALUES (1,'19:30');
 -- TRUNCATE TABLE session;
 */
 
--- Pruebas session
-
+-- Pruebas athlete_session_per_day
 /*
 INSERT INTO class(id_class,id_wod, date) VALUES (1, NULL, '2020-03-16');
 INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (1, 'Andre', null, null, null, null, null, null, null);
 INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (1, 1, 1, '5:00');
-SELECT COUNT(s.id_session) FROM athlete AS a, class AS c, session as s WHERE c.id_class=1 AND a.id_athlete=1;
+SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=1 AND a.id_athlete=1;
 INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (2, 1, 1, '6:00');
-SELECT COUNT(s.id_session) FROM athlete AS a, class AS c, session as s WHERE c.id_class=1 AND a.id_athlete=1;
+SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=1 AND a.id_athlete=1;
+
+INSERT INTO class(id_class,id_wod, date) VALUES (2, NULL, '2020-03-17');
+INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (3, 1, 2, '5:00');
+SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=2 AND a.id_athlete=1;
+INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (4, 1, 2, '6:00');
+SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=2 AND a.id_athlete=1;
+*/
+
+
+-- Query para unir 3 tablas (session, class, athlete)
+/*
+SELECT
+       s.id_session, c.id_class,a.id_athlete
+FROM
+     session as s
+JOIN
+     class as c ON s.id_class=c.id_class
+JOIN
+    athlete as a ON s.id_athlete = a.id_athlete
+WHERE
+    a.id_athlete = 1
+        AND
+    c.id_class = 2;
 */
