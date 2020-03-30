e234f7bca14e051b46f59ff53312d5370168f4a9
AndreRodas <andrejosue.rh@gmail.com>
2020-03-29 19:08:39 -0600

    V0.4

--- schema_v0.1.sql
+++ schema_v0.1.sql
@@ -1,8 +1,9 @@
+DROP DATABASE IF EXISTS crossfit;
 CREATE DATABASE crossfit;
 
 USE crossfit;
 
-CREATE TABLE `wod` (
+CREATE TABLE  `wod` (
   `id_wod` INT AUTO_INCREMENT,
   `name` VARCHAR(25),
   `description` VARCHAR(500),
@@ -10,42 +11,6 @@ CREATE TABLE `wod` (
   `type` ENUM ('The Girls','The Heroes','Normal'),
   `date` DATE,
   PRIMARY KEY (`id_wod`)
-);
-
-CREATE TABLE `personal_records_sp` (
-  `id_pr_sp` INT AUTO_INCREMENT,
-  `id_athlete` INT,
-  `id_specialty` INT,
-  `dia` TIMESTAMP,
-  `value` DOUBLE,
-  PRIMARY KEY (`id_pr_sp`),
-  CONSTRAINT `fk_id_athlete` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `fk_id_specialty` FOREIGN KEY (id_specialty) REFERENCES specialty (id_specialty) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE `rxgoals` (
-  `id_rxgoals` INT AUTO_INCREMENT,
-  `id_wod` INT,
-  `sex` ENUM ('M','F'),
-  `rx` INT,
-  `rxplus` INT,
-  `dim` ENUM ('kgs','sec','reps','rounds'),
-  PRIMARY KEY (`id_rxgoals`),
-  CONSTRAINT `fk_id_wod` FOREIGN KEY (id_wod) REFERENCES wod (id_wod) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-CREATE TABLE `class` (
-  `id_class` INT AUTO_INCREMENT,
-  `id_wod` INT,
-  `date` DATE,
-  PRIMARY KEY (`id_class`),
-  CONSTRAINT `fk_id_wod` FOREIGN KEY (id_wod) REFERENCES wod (id_wod) ON DELETE set null ON UPDATE CASCADE
-);
-
-CREATE TABLE `warmup` (
-  `id_warmup` INT AUTO_INCREMENT,
-  `description` VARCHAR(500),
-  PRIMARY KEY (`id_warmup`)
 );
 
 CREATE TABLE `athlete` (
@@ -61,6 +26,12 @@ CREATE TABLE `athlete` (
   PRIMARY KEY (`id_athlete`)
 );
 
+CREATE TABLE `warmup` (
+  `id_warmup` INT AUTO_INCREMENT,
+  `description` VARCHAR(500),
+  PRIMARY KEY (`id_warmup`)
+);
+
 CREATE TABLE `specialty` (
   `id_specialty` INT AUTO_INCREMENT,
   `description` VARCHAR(500),
@@ -69,11 +40,42 @@ CREATE TABLE `specialty` (
   PRIMARY KEY (`id_specialty`)
 );
 
+CREATE TABLE `personal_records_sp` (
+  `id_pr_sp` INT AUTO_INCREMENT,
+  `id_athlete` INT,
+  `id_specialty` INT,
+  `dia` TIMESTAMP,
+  `value` DOUBLE,
+  PRIMARY KEY (`id_pr_sp`),
+  CONSTRAINT `fk_id_athlete_pra` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_specialty_prs` FOREIGN KEY (id_specialty) REFERENCES specialty (id_specialty) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `rxgoals` (
+  `id_rxgoals` INT AUTO_INCREMENT,
+  `id_wod` INT,
+  `sex` ENUM ('M','F'),
+  `rx` INT,
+  `rxplus` INT,
+  `dim` ENUM ('kgs','sec','reps','rounds'),
+  PRIMARY KEY (`id_rxgoals`),
+  CONSTRAINT `fk_id_wod_rw` FOREIGN KEY (id_wod) REFERENCES wod (id_wod) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `class` (
+  `id_class` INT AUTO_INCREMENT,
+  `id_wod` INT,
+  `date` DATE,
+  `day_week` INT GENERATED ALWAYS AS (DAYOFWEEK(date)),
+  PRIMARY KEY (`id_class`),
+  CONSTRAINT `fk_id_wod_cw` FOREIGN KEY (id_wod) REFERENCES wod (id_wod) ON DELETE set null ON UPDATE CASCADE
+);
+
 CREATE TABLE `coach` (
   `id_coach` INT AUTO_INCREMENT,
-  `id_athelte` INT,
+  `id_athlete` INT,
   PRIMARY KEY (`id_coach`),
-  CONSTRAINT `fk_id_athelte` FOREIGN KEY (id_athelte) REFERENCES athlete (id_athlete) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_id_athlete_ca` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `personal_records_wod` (
@@ -83,8 +85,8 @@ CREATE TABLE `personal_records_wod` (
   `dia` TIMESTAMP,
   `value` DOUBLE,
   PRIMARY KEY (`id_pr_wod`),
-  CONSTRAINT `fk_id_wod` FOREIGN KEY (id_wod) REFERENCES wod (id_wod) ON DELETE SET NULL ON UPDATE    CASCADE,
-  CONSTRAINT `fk_id_athelte` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `fk_id_wod_pw` FOREIGN KEY (id_wod) REFERENCES wod (id_wod) ON DELETE SET NULL ON UPDATE    CASCADE,
+  CONSTRAINT `fk_id_athlete_pa` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE `session` (
@@ -99,11 +101,10 @@ CREATE TABLE `session` (
   `specialty_score` INT,
   `hour` ENUM ('8:00','6:00','7:00','9:30','11:00','12:00','16:30','17:30','18:30'),
   PRIMARY KEY (`id_session`),
-  CONSTRAINT `fk_id_specialty` FOREIGN KEY (id_specialty) REFERENCES specialty (id_specialty) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_id_warmup` FOREIGN KEY (id_warmup) REFERENCES warmup (id_warmup) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_id_athlete` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_id_coach` FOREIGN KEY (id_coach) REFERENCES coach (id_coach) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_id_class` FOREIGN KEY (id_class) REFERENCES class (id_class) ON DELETE SET NULL ON UPDATE CASCADE
-
+  CONSTRAINT `fk_id_specialty_ss` FOREIGN KEY (id_specialty) REFERENCES specialty (id_specialty) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_warmup_sw` FOREIGN KEY (id_warmup) REFERENCES warmup (id_warmup) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_athlete_sa` FOREIGN KEY (id_athlete) REFERENCES athlete (id_athlete) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_coach_sc` FOREIGN KEY (id_coach) REFERENCES coach (id_coach) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `fk_id_class_sc` FOREIGN KEY (id_class) REFERENCES class (id_class) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
