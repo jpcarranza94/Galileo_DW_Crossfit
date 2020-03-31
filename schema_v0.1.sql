22a5885ed2d9f10a580c768fe42cbf2849d4ea9f
AndreRodas <andrejosue.rh@gmail.com>
2020-03-31 11:11:56 -0600

    V1.8
    Ya con 'Un coach no puede tener más de 3 clases por dia' testeado

--- schema_v0.1.sql
+++ schema_v0.1.sql
@@ -133,16 +133,19 @@ CREATE TRIGGER athlete_session_per_day
         IF ((SELECT s.id_session FROM session as s JOIN class as c ON s.id_class=c.id_class JOIN athlete as a ON s.id_athlete = a.id_athlete
         WHERE a.id_athlete = new.id_athlete AND c.id_class = new.id_class) IS NOT NULL) THEN
            signal sqlstate '45000' SET MESSAGE_TEXT = 'Solamente se puede una clase por dia';
+        ELSEIF ((SELECT COUNT(DISTINCT s.hour) FROM session as s WHERE s.id_coach=new.id_coach and s.id_class=new.id_class) = 3 ) AND
+        ((SELECT DISTINCT se2.hour FROM  session as se2 WHERE se2.hour = new.hour) IS NULL )  THEN
+           signal sqlstate '45000' SET MESSAGE_TEXT = 'Un coach no puede tener más de 3 clases por dia';
         END IF;
     END;
 //
 Delimiter ;
 
-
+-- ((SELECT se2.hour FROM  session as se2 WHERE se2.hour = new.hour) IS NULL )
 -- Pruebas
 
 
--- Pruebas day_validation
+-- Pruebas day_validation ------------------------------------------------------------------------------
 
 /*
 INSERT INTO class(id_wod, date) VALUES (NULL, '2020-03-22')
@@ -159,8 +162,9 @@ INSERT INTO  session(id_class,hour) VALUES (1,'19:30');
 -- TRUNCATE TABLE session;
 */
 
--- Pruebas athlete_session_per_day
+-- Pruebas athlete_session_per_day ----------------------------------------------------------------------
 /*
+-- Prueba 'Solamente se puede una clase por dia'
 INSERT INTO class(id_class,id_wod, date) VALUES (1, NULL, '2020-03-16');
 INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (1, 'Andre', null, null, null, null, null, null, null);
 INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (1, 1, 1, '5:00');
@@ -173,6 +177,26 @@ INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (3, 1, 2, '5
 SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=2 AND a.id_athlete=1;
 INSERT INTO  session(id_session, id_athlete, id_class, hour) VALUES (4, 1, 2, '6:00');
 SELECT s.id_athlete FROM athlete AS a, class AS c, session AS s WHERE c.id_class=2 AND a.id_athlete=1;
+*/
+/*
+-- Prueba 'Un coach no puede tener más de 3 clases por dia'
+INSERT INTO class(id_class,id_wod, date) VALUES (1, NULL, '2020-03-16');
+INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (1, 'ElCoach', null, null, null, null, null, null, null);
+INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (2, 'Andre', null, null, null, null, null, null, null);
+INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (3, 'JuanPa', null, null, null, null, null, null, null);
+INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (4, 'Preng', null, null, null, null, null, null, null);
+INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (5, 'Marlon', null, null, null, null, null, null, null);
+INSERT INTO athlete (id_athlete, name, weight, height, age, sex, solvency, telephone, DPI) VALUES (6, 'Luis', null, null, null, null, null, null, null);
+
+INSERT INTO coach (id_coach, id_athlete) VALUES (1,1);
+INSERT INTO  session(id_session, id_coach, id_class, id_athlete, hour) VALUES (1, 1, 1, 2, '5:00');
+SELECT id_session,id_athlete,id_coach,id_class,hour FROM session;
+INSERT INTO  session(id_session, id_coach, id_class, id_athlete, hour) VALUES (2, 1, 1, 3, '6:00');
+INSERT INTO  session(id_session, id_coach, id_class, id_athlete, hour) VALUES (3, 1, 1, 4, '6:00');
+INSERT INTO  session(id_session, id_coach, id_class, id_athlete, hour) VALUES (4, 1, 1, 5, '12:00');
+INSERT INTO  session(id_session, id_coach, id_class, id_athlete, hour) VALUES (5, 1, 1, 6, '18:30');
+SELECT id_session,id_athlete,id_coach,id_class,hour FROM session;
+INSERT INTO  session(id_session, id_coach, id_class, id_athlete, hour) VALUES (5, 1, 1, 6, '12:00'); -- Este si lo debe de agregar
 */
 
 
